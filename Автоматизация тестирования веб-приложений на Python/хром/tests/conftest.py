a5d319db8f43b13d073cd2f26dee28f6cceac481
Natasha <115388396+Pustay@users.noreply.github.com>
2023-11-02 18:52:12 +0900

    Update conftest.py

--- Автоматизация тестирования веб-приложений на Python/хром/tests/conftest.py
+++ Автоматизация тестирования веб-приложений на Python/хром/tests/conftest.py
@@ -5,6 +5,7 @@ from yaml_reader import data
 
 @fixture(scope='function')
 def driver():
+    """google Chrome is used as driver"""
     driver = webdriver.Chrome(service=ChromeService(data['driver_path']))
     driver.maximize_window()
     yield driver
