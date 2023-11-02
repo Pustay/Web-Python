120392be6284c37cca8398785bb8c7ae5b9b9151
Natasha <115388396+Pustay@users.noreply.github.com>
2023-11-02 18:41:36 +0900

    Update contact_us_locators.py

--- Автоматизация тестирования веб-приложений на Python/хром/locators/contact_us_locators.py
+++ Автоматизация тестирования веб-приложений на Python/хром/locators/contact_us_locators.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from chrome.locators.base_locators import BaseLocators
 
 class ContactUsPageLocators(BaseLocators):
+     """Class for Contact us page locators."""
     INPUT_NAME = (By.CSS_SELECTOR, '[type="text"]')
     INPUT_EMAIL = (By.XPATH, '//*[@type="email"]')
     INPUT_CONTENT = (By.CSS_SELECTOR, 'textarea.mdc-text-field__input')
