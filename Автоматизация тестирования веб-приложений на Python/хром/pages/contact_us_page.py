8e1f687da5a17302eccf21e1363cabddd6567be9
Natasha <115388396+Pustay@users.noreply.github.com>
2023-11-02 18:49:07 +0900

    Update contact_us_page.py

--- Автоматизация тестирования веб-приложений на Python/хром/pages/contact_us_page.py
+++ Автоматизация тестирования веб-приложений на Python/хром/pages/contact_us_page.py
@@ -1,7 +1,9 @@
+"""This module represents contact us page"""
 from chrome.locators.contact_us_locators import ContactUsPageLocators
 from chrome.pages.base_page import BasePage
 
 class ContactUsPage(BasePage):
+    """Contact us page"""
     locators = ContactUsPageLocators()
 
     input_locators: list[tuple[str, str]] = [
@@ -11,12 +13,14 @@ class ContactUsPage(BasePage):
     ]
 
     def fill_contact_us_form(self, name: str, email: str, content: str):
+        """Fill the contact us form"""
         for text, locator in zip([name, email, content], self.input_locators):
             self.element_is_clickable(
                 locator=locator
             ).send_keys(text)
 
     def submit_form(self):
+        """Submit the contact us form"""
         self.element_is_clickable(
             locator=self.locators.SUBMIT_BUTTON
         ).click()
