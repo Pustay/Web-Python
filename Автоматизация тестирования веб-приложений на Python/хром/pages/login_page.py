24911dae4b8867547f00f007019311678ea75862
Natasha <115388396+Pustay@users.noreply.github.com>
2023-11-02 18:50:16 +0900

    Update login_page.py

--- Автоматизация тестирования веб-приложений на Python/хром/pages/login_page.py
+++ Автоматизация тестирования веб-приложений на Python/хром/pages/login_page.py
@@ -1,12 +1,15 @@
+"""This module represents login page"""
 from yaml_reader import data
 from chrome.locators.login_page_locators import LoginPageLocators
 from chrome.pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
+    """Login page"""
     locators = LoginPageLocators()
 
     def authorize(self) -> None:
+        """Open the page login page and enter creds"""
         self.open()
         self.element_is_clickable(self.locators.INPUT_USERNAME).send_keys(data['username_task2'])
         self.element_is_clickable(self.locators.INPUT_PASSWORD).send_keys(data['password_task2'])
